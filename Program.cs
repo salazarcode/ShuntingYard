938dae39c46933718905ce97f417d56fb4d5d465
Andrés Salazar <salazarcode@gmail.com>
2024-11-29 15:09:29 -0400

    Olvidé poner parámetros al constructor luego del cambio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using ExpressionEvaluation;
+using System.Globalization;
 using System.Linq.Expressions;
 
 namespace ShuntingYard
@@ -7,14 +8,14 @@ namespace ShuntingYard
     {
         static void Main(string[] args)
         {
-            ExpressionEvaluator evaluator = new ExpressionEvaluator();
-
             Dictionary<string, double> variables = new Dictionary<string, double>()
             {
                 {"estudiantes", 6},
                 {"interes", 26},
                 {"peso", 16.5 }
             };
+            ExpressionEvaluator evaluator = new ExpressionEvaluator(variables);
+
                 
             Console.WriteLine($"Variables:---------------------------start");
 
@@ -30,9 +31,9 @@ namespace ShuntingYard
 
             while (true) { 
                 Console.WriteLine("Introduce una expresion para evaluarla con las variables preconfiguradas: ");
-                var expression = Console.ReadLine();
+                string expression = Console.ReadLine() ?? "";
 
-                double result = evaluator.EvaluateExpression(expression, variables);
+                double result = evaluator.Evaluate(expression);
 
                 Console.WriteLine($"Expresión: {expression}");
                 Console.WriteLine($"Resultado: {result}");
