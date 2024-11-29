1f954c4077f9a41ac94ef84c0a0df171d0f5ea5d
Andrés Salazar <salazarcode@gmail.com>
2024-11-29 17:54:43 -0400

    Constructor de sobra

--- ShuntingYardEvaluator/ExpressionEvaluator.cs
+++ ShuntingYardEvaluator/ExpressionEvaluator.cs
@@ -5,18 +5,12 @@ namespace ShuntingYardEvaluator
 {
     public class ExpressionEvaluator
     {
-        private readonly Precedence _precedence;
-        private readonly Dictionary<string, double> _defaultVariables = new();
-
-        public ExpressionEvaluator()
-        {
-            _precedence = new Precedence();
-        }
+        private readonly Precedence _precedence = new Precedence();
 
         public double EvaluateFromInfixExpression(string expression, Dictionary<string, double>? _variables = null)
         {
             string postfixExpression = new InfixToPostfixParser(_precedence).ConvertToPostfix(expression);
-            var res = new ShuntingYardAlgorithm(_precedence).EvaluatePostfix(postfixExpression, _variables ?? _defaultVariables);
+            var res = new ShuntingYardAlgorithm(_precedence).EvaluatePostfix(postfixExpression, _variables ?? new Dictionary<string, double>());
             return res;
         }
     }
