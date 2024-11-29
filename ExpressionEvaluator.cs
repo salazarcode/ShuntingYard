79f6781b4e0dcf898e75b47e7181c901d9f84d71
Andrés Salazar <salazarcode@gmail.com>
2024-11-29 15:03:43 -0400

    Add project files.

---
+++ ExpressionEvaluator.cs
@@ -0,0 +1,187 @@
+﻿using System.Globalization;
+
+namespace ShuntingYard
+{
+
+    public class ExpressionEvaluator
+    {
+        private Dictionary<char, int> precedencia;
+
+        public ExpressionEvaluator()
+        {
+            precedencia = new Dictionary<char, int>()
+            {
+                {'^', 4},
+                {'*', 3},
+                {'/', 3},
+                {'%', 3},
+                {'+', 2},
+                {'-', 2},
+                {'(', 1}
+            };
+        }
+
+        public double EvaluateExpression(string expression, Dictionary<string, double> variables)
+        {
+            string postfix = ConvertToPostfix(expression);
+            return EvaluatePostfix(postfix, variables);
+        }
+
+        private string ConvertToPostfix(string infix)
+        {
+            Stack<string> pilaOperadores = new Stack<string>();
+            List<string> salida = new List<string>();
+
+            int i = 0;
+            while (i < infix.Length)
+            {
+                char token = infix[i];
+
+                if (Char.IsWhiteSpace(token))
+                {
+                    i++;
+                    continue;
+                }
+
+                if (Char.IsDigit(token) || token == '.')
+                {
+                    string operando = "";
+                    bool hayPuntoDecimal = false;
+
+                    while (i < infix.Length && (Char.IsDigit(infix[i]) || infix[i] == '.'))
+                    {
+                        if (infix[i] == '.')
+                        {
+                            if (hayPuntoDecimal)
+                            {
+                                throw new Exception("Número mal formado: más de un punto decimal.");
+                            }
+                            hayPuntoDecimal = true;
+                        }
+                        operando += infix[i];
+                        i++;
+                    }
+                    salida.Add(operando);
+                    continue;
+                }
+                else if (Char.IsLetter(token))
+                {
+                    // Manejo de variables (letras o palabras)
+                    string variable = "";
+                    while (i < infix.Length && Char.IsLetterOrDigit(infix[i]))
+                    {
+                        variable += infix[i];
+                        i++;
+                    }
+                    salida.Add(variable);
+                    continue;
+                }
+                else if (token == '(')
+                {
+                    pilaOperadores.Push(token.ToString());
+                    i++;
+                }
+                else if (token == ')')
+                {
+                    while (pilaOperadores.Count > 0 && pilaOperadores.Peek() != "(")
+                    {
+                        salida.Add(pilaOperadores.Pop());
+                    }
+                    if (pilaOperadores.Count == 0)
+                    {
+                        throw new Exception("Paréntesis desbalanceados: falta '('");
+                    }
+                    pilaOperadores.Pop(); // Eliminar '(' de la pila
+                    i++;
+                }
+                else // Operador
+                {
+                    if (!precedencia.ContainsKey(token))
+                    {
+                        throw new Exception($"Operador desconocido: {token}");
+                    }
+                    string opToken = token.ToString();
+                    while (pilaOperadores.Count > 0 && precedencia.ContainsKey(pilaOperadores.Peek()[0]) && precedencia[pilaOperadores.Peek()[0]] >= precedencia[token])
+                    {
+                        salida.Add(pilaOperadores.Pop());
+                    }
+                    pilaOperadores.Push(opToken);
+                    i++;
+                }
+            }
+
+            while (pilaOperadores.Count > 0)
+            {
+                if (pilaOperadores.Peek() == "(" || pilaOperadores.Peek() == ")")
+                {
+                    throw new Exception("Paréntesis desbalanceados.");
+                }
+                salida.Add(pilaOperadores.Pop());
+            }
+
+            return string.Join(" ", salida);
+        }
+
+        private double EvaluatePostfix(string postfix, Dictionary<string, double> variables)
+        {
+            Stack<double> stack = new Stack<double>();
+            string[] tokens = postfix.Split(' ');
+
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                {
+                    stack.Push(number);
+                }
+                else if (IsOperator(token))
+                {
+                    if (stack.Count < 2)
+                    {
+                        throw new Exception("Expresión postfija mal formada.");
+                    }
+
+                    double b = stack.Pop();
+                    double a = stack.Pop();
+
+                    double result = token switch
+                    {
+                        "+" => a + b,
+                        "-" => a - b,
+                        "*" => a * b,
+                        "/" => a / b,
+                        "%" => a % b,
+                        "^" => Math.Pow(a, b),
+                        _ => throw new Exception($"Operador desconocido: {token}")
+                    };
+
+                    stack.Push(result);
+                }
+                else
+                {
+                    // Asumimos que es una variable
+                    if (variables.ContainsKey(token))
+                    {
+                        stack.Push(variables[token]);
+                    }
+                    else
+                    {
+                        throw new Exception($"Variable no definida: {token}");
+                    }
+                }
+            }
+
+            if (stack.Count != 1)
+            {
+                throw new Exception("Expresión postfija mal formada.");
+            }
+
+            return stack.Pop();
+        }
+
+        private bool IsOperator(string token)
+        {
+            return token == "+" || token == "-" || token == "*" || token == "/" || token == "%" || token == "^";
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+
+namespace ShuntingYard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionEvaluator evaluator = new ExpressionEvaluator();
+
+            Dictionary<string, double> variables = new Dictionary<string, double>()
+            {
+                {"estudiantes", 6},
+                {"interes", 26},
+                {"peso", 16.5 }
+            };
+                
+            Console.WriteLine($"Variables:---------------------------start");
+
+            foreach (var item in variables)
+            {
+
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+            Console.WriteLine($"Variables:---------------------------end");
+
+            Console.WriteLine("");
+
+            while (true) { 
+                Console.WriteLine("Introduce una expresion para evaluarla con las variables preconfiguradas: ");
+                var expression = Console.ReadLine();
+
+                double result = evaluator.EvaluateExpression(expression, variables);
+
+                Console.WriteLine($"Expresión: {expression}");
+                Console.WriteLine($"Resultado: {result}");
+            }
+        }
+    }
+}
+
